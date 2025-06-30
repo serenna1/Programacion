9f92e1a9c747bda2c64e407bacabdcb54399d978
serenna1 <rotondaserena3@gmail.com>
2025-06-30 01:00:07 -0300

    Ignoro node_modules y package-lock.json correctamente

--- TP2/Ejercicios/ejercicio2.js
+++ TP2/Ejercicios/ejercicio2.js
@@ -1,15 +1,19 @@
 //Realiza una aplicación que le solicite al usuario su nombre y su edad y le muestre por
 //pantalla el mensaje “Hola XX tu edad es XX” donde XX son los datos del usuario.
-const prompt = require (prompt-sync)();
+const prompt = require ("prompt-sync")();
 let usuario;
+let nombre;
 usuario = prompt ("Ingrese su nombre: ")
 while (usuario !== "fin"){
     console.log (`Hola ${usuario}`);
-    usuario = prompt ("Ingrese su edad: ");
-    console.log (`Su edad  es: ${usuario}`);
-    usuario = prompt ("escriba fin")
+    nombre = usuario;
+    usuario = prompt ( `${nombre} a continuacion escriba su Edad: `);
+    console.log (`Su Edad es: ${usuario}`);
+    usuario = prompt ("escriba fin: ")
     
     if(usuario === "fin"){
         console.log(`Gracias por dejarnos sus datos. `);
     }
+    
 }
+console.log(`Escribio fin, vuelva a intentarlo. `)
