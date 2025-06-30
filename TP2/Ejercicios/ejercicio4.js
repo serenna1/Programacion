3a2e218c06f3d02d6ac1b8afbae5e5d873a4dfa9
serenna1 <rotondaserena3@gmail.com>
2025-06-30 11:19:51 -0300

    Commit de ejerc.4

---
+++ TP2/Ejercicios/ejercicio4.js
@@ -0,0 +1,23 @@
+//Realiza una calculadora con las operaciones básicas 
+// (suma, resta multiplicación y división).
+//Los valores para las operaciones deben estar almacenados en variables 
+// previamente.
+
+let num1 = 3;
+let num2 = 5;
+let num3 = 10;
+let num4 = 4;
+resutado = 0;
+
+
+resultado = num3 + num1;
+console.log (`La suma de ${num3} y ${num1} es: ${resultado}`);
+resultado = 0;
+resultado = num4 - num2;
+console.log(`La resta de ${num4} y ${num2} es: ${resultado}`);
+resultado = 0;
+resultado = num3 / num2;
+console.log(`La division de ${num3} y ${num2} es: ${resultado}`);
+resultado = 0; 
+resultado = num1 * num4;
+console.log(`La multiplicacion de ${num1} y ${num4} es: ${resultado}`);
