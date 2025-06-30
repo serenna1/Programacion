ed621d8c2de0b01459d041fb7b669d66b53f8cb6
serenna1 <rotondaserena3@gmail.com>
2025-06-30 11:09:45 -0300

    Commit del ejer.3

---
+++ TP2/Ejercicios/ejercicio3.js
@@ -0,0 +1,8 @@
+//Realiza una aplicación que dados dos valores colocados en variables realice la suma de los
+//mismos y los muestre por pantalla.
+let num1 = 5;
+let num2 = 5;
+let resultado = 0;
+
+resultado = num1 + num2;
+console.log(`La suma de ${num1} y ${num2} es: ${resultado}`);
