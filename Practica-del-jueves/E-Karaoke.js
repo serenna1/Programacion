8a6118c6bf5c63fe85164d8c517f4a4371ae14bf
serenna1 <rotondaserena3@gmail.com>
2025-09-04 21:28:18 -0300

    subiendo ejercicio karaoke, hay que modificarlo

---
+++ Practica-del-jueves/E-Karaoke.js
@@ -0,0 +1,66 @@
+/*
+-escribir una funcion que reciba un texto y devuelva solo las vocales de este texto
+-escribir una funcion que cuente cuantas vocales tiene una palabra
+-escribir una funcion que reciba dos frases que diga cual tiene mas vocales
+
+*/
+//hola
+function devolverVocales(frase) {
+  let vocales = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
+  let newArray = [];
+  let newPos = 0;
+  for (pos of frase) {
+    for (let i = 0; i < vocales.length; i++) {
+      if (pos === vocales[i]) {
+        newArray[newPos] = pos;
+        newPos++;
+      }
+    }
+  }
+  console.log(newArray);
+}
+devolverVocales("hola ea");
+
+function devolverVocalCantidad(frase) {
+  let vocales = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
+  let cantidadDeVocales = 0;
+  for (pos of frase) {
+    for (let i = 0; i < vocales.length; i++) {
+      if (pos === vocales[i]) {
+        cantidadDeVocales++;
+      }
+    }
+  }
+  console.log(`las cantidades de vocales son: ${cantidadDeVocales}`);
+}
+devolverVocalCantidad("hola ea");
+
+function cualTieneMasVocales(frase1, frase2) {
+  let vocales = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
+  let cantidadDeVocalesF1 = 0;
+  let cantidadDeVocalesF2 = 0;
+  for (pos of frase1) {
+    for (let i = 0; i < vocales.length; i++) {
+      if (pos === vocales[i]) {
+        cantidadDeVocalesF1++;
+      }
+    }
+  }
+  for (pos of frase2) {
+    for (let i = 0; i < vocales.length; i++) {
+      if (pos === vocales[i]) {
+        cantidadDeVocalesF2++;
+      }
+    }
+  }
+  console.log(`las frases a comparar son: ${frase1}, ${frase2}`);
+  if (cantidadDeVocalesF1 > cantidadDeVocalesF2) {
+    console.log(
+      `la frase que tiene mas vocales es: ${frase1}, con ${cantidadDeVocalesF1} vocales`
+    );
+  } else
+    console.log(
+      `la frase que tiene mas vocales es: ${frase2},con ${cantidadDeVocalesF2} vocales`
+    );
+}
+cualTieneMasVocales("hola ea", "aHhvcanaae");
